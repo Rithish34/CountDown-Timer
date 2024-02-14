03c4c50e97f26e69788f4ec591e172591d1fb66a
Rithish kumar <150705896+Rithish34@users.noreply.github.com>
2024-02-14 21:24:10 +0530

    Add files via upload
    Countdown Timer in Pure HTML CSS and JS

---
+++ script.js
@@ -0,0 +1,22 @@
+const days=document.querySelector("#days");
+const hours=document.querySelector("#hours");
+const mintues=document.querySelector("#mintues");
+const seconds=document.querySelector("#seconds");
+
+
+
+function UpdateTime(){
+const currentyear=new Date().getFullYear();
+const newyear=new Date(`January 1 ${currentyear+1} 00:00:00`);
+const currentDate=new Date();
+const diff=newyear-currentDate;
+const d=Math.floor(diff/1000/60/60/24);
+const h=Math.floor((diff/1000/60/60)%24);
+const m=Math.floor((diff/1000/60)%60);
+const s=Math.floor((diff/1000)%60);
+days.innerHTML=d<10?"0"+d:d;
+hours.innerHTML=h<10?"0"+h:h;
+mintues.innerHTML=m<10?"0"+m:m;
+seconds.innerHTML=s<10?"0"+s:s;
+}
+ setInterval(UpdateTime,1000);
